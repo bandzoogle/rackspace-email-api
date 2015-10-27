d1a6673565f538491de00cd97be9805f99eb3b2e
Colin Mitchell <muffinista@gmail.com>
2015-10-27 16:50:16 -0400

    code to generate mailbox login_tokens

--- lib/rackspace/email/api/mailbox.rb
+++ lib/rackspace/email/api/mailbox.rb
@@ -22,4 +22,27 @@ class Rackspace::Email::Api::Mailbox < Rackspace::Email::Api::ApiObject
 		[name, domain].join("@")
 	end
 
+# post '/customers/460896/domains/testapidomain.net/rs/mailboxes/testmailbox/loginToken', {}, 'text/xml'
+
+	def login_token
+		url = Rackspace::Email::Api.configuration.base_url.dup +
+			"/customers/#{accountNumber}/domains/#{domain}/rs/mailboxes/#{name}/loginToken"		
+
+		conn = Faraday.new(url) do |c|
+			c.response :logger, ::Logger.new(STDOUT) #, bodies: true
+			c.use Faraday::Request::UrlEncoded
+			c.request :url_encoded
+			c.adapter Faraday.default_adapter
+		end
+
+		x = conn.post do |req|
+			req.headers['User-Agent'] = Rackspace::Email::Api.configuration.user_agent
+			req.headers['X-Api-Signature'] = Rackspace::Email::Api::Endpoint.new.api_signature
+			req.headers['Accept'] = Rackspace::Email::Api.configuration.response_format
+		end
+
+		JSON.parse(x.body)["token"]
+	end
+
+
 end
