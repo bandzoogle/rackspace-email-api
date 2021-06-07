bad410706a78e0a055a971e3d815c66df60b0326
Colin Mitchell <muffinista@gmail.com>
2021-06-07 15:51:55 -0400

    Add enable/disable commands

--- lib/rackspace/email/api/customer.rb
+++ lib/rackspace/email/api/customer.rb
@@ -11,6 +11,37 @@ module Rackspace
         def domains
           Rackspace::Email::Api::Domains.new(accountNumber)
         end
+
+        def post_url(url)
+          uri = URI(url)
+          req = Net::HTTP::Post.new(uri,
+                                    'User-Agent' => Rackspace::Email::Api.configuration.user_agent,
+                                    'X-Api-Signature' => Rackspace::Email::Api::Endpoint.new.api_signature,
+                                    'Accept' => Rackspace::Email::Api.configuration.response_format)
+
+          res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme.casecmp('https').zero?) do |http|
+            http.request(req)
+          end
+
+          JSON.parse(res.body)
+        end
+
+        def disable
+          url = [
+            Rackspace::Email::Api.configuration.base_url.dup,
+            "customers/#{accountNumber}/disable"
+          ].join('/')
+          post_url(url)
+        end
+
+        def enable
+          url = [
+            Rackspace::Email::Api.configuration.base_url.dup,
+            "customers/#{accountNumber}/enable"
+          ].join('/')
+          post_url(url)
+        end
+
       end
     end
   end
